fca3ecaec62722fd3a30d2bbf7f2f5f352504a7c
luks22 <luksilva1796@gamil.com>
2020-02-21 18:18:35 -0300

    segunda versao

--- Calculadora.java
+++ Calculadora.java
@@ -7,13 +7,21 @@ public class Calculadora {
 		
 		
 		do{
-			option = Integer.parseInt(JOptionPane.showInputDialog("Escolha:" +"\n"+"1 - soma"+"\n"+"0 - sair"));
+			option = Integer.parseInt(JOptionPane.showInputDialog("Escolha:" +"\n"+"0 - sair"+"\n"+""1 - soma""+
+"\n"+"2 - subtração"));
 			
 			if(option == 1) {
 				firstNum = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
 				secondNum = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
 				result = firstNum + secondNum;
 				JOptionPane.showMessageDialog(null, "O resultado da soma é " + result);
+			}
+
+			if(option == 2) {
+				firstNum = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
+				secondNum = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
+				result = firstNum - secondNum;
+				JOptionPane.showMessageDialog(null, "O resultado da subtração é " + result);
 			}
 		}while(option != 0);
 
